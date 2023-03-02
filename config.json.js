bae10c1c77aa8fa73ab8e13b64121302b8dc3e9c
Jamcz <1>
2023-03-03 00:12:37 +0800

    init

---
+++ config.json.js
@@ -0,0 +1,12 @@
+{
+"version":"1.6",
+"md5":"8a9f14713ca4fd0a7a9a44d3a9dad097",
+"home_url":"Module_fastd1ck.asp",
+"title":"迅雷快鸟 - 增强版",
+"description":"迅雷快鸟，上网必备神器",
+"tags":"网络",
+"author":"sadog",
+"link":"",
+"changelog":"",
+"build_date":"2023-03-02_23:50:14"
+}
